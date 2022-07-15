ec89efd540fbe63e8860f4c53a382071b9254f1c
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2022-07-15 22:53:31 +0200

    Initial dbcontext configuration, tests os bootstrap

---
+++ MinecraftServerWeb/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinecraftServerWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
+    }
+}
--- MinecraftServerWeb/Program.cs
+++ MinecraftServerWeb/Program.cs
@@ -1,7 +1,14 @@
+using MinecraftServerWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")
+    ));
+
 
 var app = builder.Build();
 
