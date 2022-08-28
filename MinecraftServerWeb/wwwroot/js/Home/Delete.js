512769e08fa998d49e4cf87aa9a5f105b570213b
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2022-08-28 15:55:34 +0200

    add Delete announcement alert

---
+++ MinecraftServerWeb/wwwroot/js/Home/Delete.js
@@ -0,0 +1,24 @@
+﻿function DeletePost(postTitle, url) {
+    Swal.fire({
+        title: `Czy na pewno chcesz usunąć post o nazwie ${postTitle}?`,
+        text: "Tej operacji nie będzie można cofnąć!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success:
+                    Swal.fire(
+                        'Sukces',
+                        `Post o nazwie: ${postTitle} został usunięty`,
+                        'success'
+                    )
+            });
+        }
+    });
+}
