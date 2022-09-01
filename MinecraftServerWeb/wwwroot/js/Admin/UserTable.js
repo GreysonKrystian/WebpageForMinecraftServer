686054e2bc958d9e1506d7f61103343e5f6c1370
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2022-09-02 00:59:21 +0200

    added primitive buttons to user list datatable

--- MinecraftServerWeb/wwwroot/js/Admin/UserTable.js
+++ MinecraftServerWeb/wwwroot/js/Admin/UserTable.js
@@ -10,11 +10,21 @@
                 render: $.fn.dataTable.render.moment('YYYY-MM-DDTHH:mm:ss.SSSSSSS', 'hh:mm:ss DD/MM/YY')
             }],
         "columns": [
-            { "data": "forumNickname", "width": "20%" },
-            { "data": "serverNickname", "width": "20%" },
+            { "data": "forumNickname", "width": "15%" },
+            { "data": "serverNickname", "width": "15%" },
             { "data": "email", "width": "20%" },
-            { "data": "rank", "width": "20%" },
-            { "data": "dateCreated", "width": "20%" }
+            { "data": "rank", "width": "10%" },
+            { "data": "dateCreated", "width": "15%" },
+            {
+                "data": "id", "width": "10%", "render": function(data) {
+                    return `
+                        <div class="row">
+                            <a class="btn btn-danger" href="/Admin/LockAccount/${data}"> Blokada </a>
+                            <a class="btn btn-warning mt-2" href="/Admin/MuteAccount/${data}"> Wyciszenie </a>
+                            <a class="btn btn-info mt-2" href="/Admin/AccountInfo/${data}"> Informacje </a>
+                        </div>`;
+                }
+            }
         ],
         "language": {
             "lengthMenu": "Wyświetl _MENU_ wyników na stronę",
