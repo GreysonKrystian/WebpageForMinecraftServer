ed52580aa1c33203be43ca1f504b7cf0a75a8ef1
Krystian Grela <krystian12340@gmail.com>
2022-09-07 15:23:43 +0200

    futher frontend improvements especially on index homepage

--- MinecraftServerWeb/Program.cs
+++ MinecraftServerWeb/Program.cs
@@ -5,12 +5,13 @@ using MinecraftServerWeb.Repository.RepositoryImplementation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using MinecraftServerWeb.Utility;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Path = MinecraftServerWeb.Utility.Path;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
     ));
