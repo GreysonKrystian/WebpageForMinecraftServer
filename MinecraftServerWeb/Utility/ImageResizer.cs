c67a387edd6092d87d5996bdf4f1419ece3678ee
Krystian Grela <73603637+GreysonKrystian@users.noreply.github.com>
2022-09-21 14:46:20 +0200

    start implementing functionality of adding profile image by user

---
+++ MinecraftServerWeb/Utility/ImageResizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace MinecraftServerWeb.Utility
+{
+    public static class ImageResizer
+    {
+        public static Bitmap ResizeImage(Size newSize, Image image)
+        {
+            Bitmap bmp = new(newSize.Width, newSize.Height);
+            bmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            var destinationRectangle = new Rectangle(0, 0, newSize.Width, newSize.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CompositingMode = CompositingMode.SourceCopy;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    g.DrawImage(image, destinationRectangle, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+                }
+            }
+            image.Dispose();
+            return bmp;
+        }
+    }
+}
---
+++ MinecraftServerWeb/wwwroot/js/User/AddImage.js
@@ -0,0 +1,31 @@
+﻿/*
+$(document).ready(function () {
+    var fileInput = $("#getFile");
+    var previewLabel = $("#previewLabel");
+    var imageName = $("#imageName");
+    fileInput[0].addEventListener("change",
+        function () {
+            previewLabel[0].style.visibility = "visible";
+            imageName[0].style.visibility = "visible";
+            imageName[0].value = fileInput.files[0].name;
+
+        });
+});
+*/
+
+
+
+function previewImage() {
+
+    var fileInput = window.$("#getFile");
+    var previewLabel = window.$("#previewLabel");
+    var imageName = window.$("#imageName");
+    fileInput[0].addEventListener("change",
+        function () {
+            previewLabel[0].style.visibility = "visible";
+            imageName[0].style.visibility = "visible";
+            imageName[0].innerHTML = "wybrane zdjęcie: " + "<strong>" + fileInput[0].files[0].name + "<\strong>";
+
+        }, false);
+    fileInput.click();
+}
